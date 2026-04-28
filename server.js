3fdbf6ed7af918a9315d91016bef8b7cb6ae6d0d
tonizhuravlevxd-blip <toni.zhuravlev.xd@mail.ru>
2026-04-28 22:19:36 +0300

    Fix REQUIRED_CHANNEL_ID_3 value in server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const REQUIRED_CHANNELS = [
     title: "Канал 2"
   },
   {
-    id: process.env.REQUIRED_CHANNEL_ID_3 || "-74076216280037437",
+    id: process.env.REQUIRED_CHANNEL_ID_3 || "-74076280037437",
     url: process.env.REQUIRED_CHANNEL_URL_3 || "https://max.ru/join/ufG4-ZgGP_lVbmSohw5ZWND7y5udP2zGDXhS7MI0pmw",
     title: "Канал 3"
   }
