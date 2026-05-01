617805b183e2c51823ccee99eb2546462f3cd8d2
tonizhuravlevxd-blip <toni.zhuravlev.xd@mail.ru>
2026-05-01 18:12:48 +0300

    Personalize greeting message with user's first name

--- server.js
+++ server.js
@@ -2095,10 +2095,17 @@ async function handleUpdate(update) {
 
   try {
     if (updateType === "bot_started") {
+      const namePrefix = firstName ? `, ${firstName}!` : "!";
+
       await sendMaxMessage(
         target,
-        "**Здравствуйте**. Напишите вопрос или попросите **создать фото/картинку/видео**. Например: создай фото кота или оживи фото."
+        `🙋🏻‍♂️ **Привет${namePrefix}**
+
+Осуществляя работу с сервисом с помощью **Max-бота**, вы подтверждаете, что ознакомлены и согласны с <a href='https://disk.yandex.ru/i/e3gVPfUa3xKyiQ'>Офертой</a> и <a href='https://disk.yandex.ru/i/LHakrABNtGiVMw'>Политикой персональных данных</a>.
+
+Напишите вопрос или попросите **создать фото/картинку/видео**. Например: создай фото кота или оживи фото.`
       );
+
       return;
     }
     const userText = getIncomingText(update);
